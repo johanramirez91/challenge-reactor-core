1520991a4846c9e17effcd1434fbbeae02f2fc7b
Johan Ramirez <johan911019@gmail.com>
2021-12-11 22:02:18 -0500

    Refactor test

--- src/test/java/com/example/demo/CSVUtilTest.java
+++ src/test/java/com/example/demo/CSVUtilTest.java
@@ -1,46 +1,18 @@
 package com.example.demo;
-
-import com.opencsv.CSVReader;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class CSVUtilTest {
+
+class CSVUtilTest {
 
     @Test
     void converterData() {
         List<Player> list = CsvUtilFile.getPlayers();
         assert list.size() == 18207;
     }
-
-    @Test
-    void stream_filtrarJugadoresMayoresA35() {
-        List<Player> list = CsvUtilFile.getPlayers();
-        Map<String, List<Player>> listFilter = list.parallelStream()
-                .filter(player -> player.age >= 35)
-                .map(player -> {
-                    player.name = player.name.toUpperCase(Locale.ROOT);
-                    return player;
-                })
-                .flatMap(playerA -> list.parallelStream()
-                        .filter(playerB -> playerA.club.equals(playerB.club))
-                )
-                .distinct()
-                .collect(Collectors.groupingBy(Player::getClub));
-
-        assert listFilter.size() == 322;
-    }
-
 
     @Test
     void reactive_filtrarJugadoresMayoresA35() {
@@ -123,13 +95,9 @@ public class CSVUtilTest {
         Objects.requireNonNull(listFilter.block()).forEach((country, players) -> {
             System.out.println("Pais: " + country);
             players.forEach(player -> {
-                System.out.println(player.name + ", numero de victorias: " + player.winners);
+                System.out.println(player.name + " victorias: " + player.winners);
             });
         });
         assert listFilter.block().size() == 164;
-    }
-
-    private Integer defaultAge(Integer integer) {
-        return integer == null ? 0 : integer;
     }
 }
